96cf946ef63bad93bcaf88eb92e0aae3e773fee0
Eliot Swank <eliotswank@gmail.com>
2018-02-27 09:43:03 -0700

    finish iteration 1

--- test/project_test.rb
+++ test/project_test.rb
@@ -9,7 +9,20 @@ class ProjectTest < Minitest::Test
   end_date = '2018-01-01'
   project = Project.new('123', 'Widget Maker', '2015-01-01', '2018-01-01')
 
-  a
+  assert_instance_of Project, project
+  end
+
+  def test_it_takes_attributes
+    project_id = '123'
+    name = 'Widget Maker'
+    start_date = '2015-01-01'
+    end_date = '2018-01-01'
+    project = Project.new('123', 'Widget Maker', '2015-01-01', '2018-01-01')
+
+    assert_equal 123, project.project_id
+    assert_equal 'Widget Maker', project.name
+    assert_equal '2015-01-01', project.start_date
+    assert_equal '2018-01-01', project.end_date
   end
 
 end
