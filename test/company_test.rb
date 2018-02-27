ca9a7b7a734a246cf283f865332f7ed4ba75225a
Eliot Swank <eliotswank@gmail.com>
2018-02-27 09:55:48 -0700

    work on iter 2

---
+++ test/company_test.rb
@@ -0,0 +1,18 @@
+require './test/test_helper'
+require './lib/company'
+
+class CompanyTest < Minitest::Test
+  def test_it_exists
+    company = Company.new
+
+    assert_instance_of Company, company
+  end
+
+  def test_it_starts_with_empty_arrays
+    company = Company.new
+
+    assert_equal [], company.employees
+    assert_equal [], company.projects
+    assert_equal [], company.timesheets  
+  end
+end
